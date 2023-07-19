89490c94e588bbcf462f624b4a8ac0f45752ac8e
Ryan Eastman <rking1323@gmail.com>
2023-07-19 11:58:04 -0700

    implementing context to save journal entries

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -8,7 +8,6 @@ import { format } from 'date-fns';
 import * as Location from 'expo-location';
 import { AppContext } from '../contexts/AppContext';
 
-
 const CameraScreen = () => {
   const [hasPermission, setHasPermission] = React.useState(null);
   const [type, setType] = React.useState(Camera.Constants.Type.back);
@@ -99,7 +98,6 @@ const CameraScreen = () => {
     return <Text>No access to camera</Text>;
   }
 
-
   return (
     <View style={styles.container}>
       <Camera style={styles.camera} type={type} ref={cameraRef}>
@@ -115,7 +113,6 @@ const CameraScreen = () => {
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
@@ -143,8 +140,156 @@ const styles = StyleSheet.create({
   },
 });
 
-
 export default CameraScreen;
+
+
+// import React, { useContext, useRef } from 'react';
+// import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+// import { Camera } from 'expo-camera';
+// import * as ImagePicker from 'expo-image-picker';
+// import * as MediaLibrary from 'expo-media-library';
+// import { useNavigation } from '@react-navigation/native';
+// import { format } from 'date-fns';
+// import * as Location from 'expo-location';
+// import { AppContext } from '../contexts/AppContext';
+
+
+// const CameraScreen = () => {
+//   const [hasPermission, setHasPermission] = React.useState(null);
+//   const [type, setType] = React.useState(Camera.Constants.Type.back);
+//   const cameraRef = useRef(null);
+//   const navigation = useNavigation();
+//   const { setJournalEntries } = useContext(AppContext); // Get the setJournalEntries function from the AppContext
+
+//   React.useEffect(() => {
+//     (async () => {
+//       const { status } = await Camera.requestPermissionsAsync();
+//       setHasPermission(status === 'granted');
+//     })();
+//   }, []);
+
+//   const handleCameraType = () => {
+//     setType(
+//       type === Camera.Constants.Type.back
+//         ? Camera.Constants.Type.front
+//         : Camera.Constants.Type.back
+//     );
+//   };
+
+//   const handleTakePhoto = async () => {
+//     if (cameraRef.current) {
+//       let photo = await cameraRef.current.takePictureAsync();
+//       const savedImageUri = await savePhoto(photo);
+
+//       // Get the current date
+//       const currentDate = format(new Date(), 'yyyy-MM-dd');
+
+//       // Get the current location
+//       let location = null;
+//       try {
+//         const { status } = await Location.requestForegroundPermissionsAsync();
+//         if (status === 'granted') {
+//           const locationData = await Location.getCurrentPositionAsync({});
+//           location = `${locationData.coords.latitude}, ${locationData.coords.longitude}`;
+//         }
+//       } catch (error) {
+//         console.log('Error getting location:', error);
+//       }
+
+//       // Create a new entry object with the photo details, date, and location
+//       const newEntry = {
+//         title: 'New Entry',
+//         date: currentDate,
+//         location: location,
+//         imageUri: savedImageUri,
+//       };
+
+//       // Add the new entry to the context's journalEntries
+//       setJournalEntries((prevEntries) => [...prevEntries, newEntry]);
+
+//       navigation.navigate('EntryScreen', { entry: newEntry });
+//     }
+//   };
+
+//   const savePhoto = async (photo) => {
+//     try {
+//       const { status } = await MediaLibrary.requestPermissionsAsync();
+
+//       if (status !== 'granted') {
+//         throw new Error('Permission not granted to access media library');
+//       }
+
+//       const asset = await MediaLibrary.createAssetAsync(photo.uri);
+//       const album = await MediaLibrary.getAlbumAsync('ExpoCameraApp');
+
+//       if (album == null) {
+//         await MediaLibrary.createAlbumAsync('ExpoCameraApp', asset, false);
+//       } else {
+//         await MediaLibrary.addAssetsToAlbumAsync([asset], album.id, false);
+//       }
+
+//       alert('Photo saved to media library!');
+//       return asset.uri;
+//     } catch (error) {
+//       console.log(error);
+//       alert('Failed to save photo to media library.');
+//     }
+//   };
+
+//   if (hasPermission === null) {
+//     return <View />;
+//   }
+
+//   if (hasPermission === false) {
+//     return <Text>No access to camera</Text>;
+//   }
+
+
+//   return (
+//     <View style={styles.container}>
+//       <Camera style={styles.camera} type={type} ref={cameraRef}>
+//         <View style={styles.buttonContainer}>
+//           <TouchableOpacity style={styles.button} onPress={handleTakePhoto}>
+//             <Text style={styles.buttonText}>Take Photo</Text>
+//           </TouchableOpacity>
+//           <TouchableOpacity style={styles.button} onPress={handleCameraType}>
+//             <Text style={styles.buttonText}>Flip</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </Camera>
+//     </View>
+//   );
+// };
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   camera: {
+//     flex: 1,
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   buttonContainer: {
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//     marginBottom: 20,
+//   },
+//   button: {
+//     margin: 10,
+//     padding: 10,
+//     borderRadius: 5,
+//     backgroundColor: '#2196F3',
+//   },
+//   buttonText: {
+//     color: 'white',
+//     fontSize: 16,
+//   },
+// });
+
+
+// export default CameraScreen;
 
 
 
--- components/EntryScreen.js
+++ components/EntryScreen.js
@@ -6,11 +6,9 @@ import { MaterialCommunityIcons } from '@expo/vector-icons'; // Import the Mater
 
 const EntryScreen = ({ route }) => {
   const navigation = useNavigation();
-  const { savedImageUri, entry } = route.params || {
-    entry: { title: '', date: '', location: '', imageUri: '' },
-  };
-  const [editedTitle, setEditedTitle] = useState(entry.title || '');
-  const { darkMode } = useContext(AppContext);
+  const { savedImageUri, entry } = route.params || { entry: { title: '', date: '', location: '', imageUri: '' } };
+  const [editedTitle, setEditedTitle] = useState(entry?.title || '');
+  const { darkMode, setJournalEntries } = useContext(AppContext); // Add setJournalEntries to update journalEntries
 
   const handleTitleChange = (text) => {
     setEditedTitle(text);
@@ -23,7 +21,20 @@ const EntryScreen = ({ route }) => {
   }, [savedImageUri]);
 
   const handleSave = () => {
+    // Create a copy of the original entry with the updated title
     const updatedEntry = { ...entry, title: editedTitle };
+
+    // Find the index of the original entry in the journalEntries array
+    const entryIndex = journalEntries.findIndex((item) => item.id === entry.id);
+
+    // Create a copy of the journalEntries array and replace the old entry with the updated one
+    const updatedEntries = [...journalEntries];
+    updatedEntries[entryIndex] = updatedEntry;
+
+    // Update the journalEntries state with the updated array
+    setJournalEntries(updatedEntries);
+
+    // Navigate back to the HomeScreen
     navigation.goBack();
   };
 
@@ -99,6 +110,109 @@ const styles = StyleSheet.create({
 });
 
 export default EntryScreen;
+
+
+// import React, { useState, useEffect, useContext } from 'react';
+// import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+// import { AppContext } from '../contexts/AppContext';
+// import { useRoute, useNavigation } from '@react-navigation/native';
+// import { MaterialCommunityIcons } from '@expo/vector-icons'; // Import the MaterialCommunityIcons
+
+// const EntryScreen = ({ route }) => {
+//   const navigation = useNavigation();
+//   const { savedImageUri, entry } = route.params || {
+//     entry: { title: '', date: '', location: '', imageUri: '' },
+//   };
+//   const [editedTitle, setEditedTitle] = useState(entry.title || '');
+//   const { darkMode } = useContext(AppContext);
+
+//   const handleTitleChange = (text) => {
+//     setEditedTitle(text);
+//   };
+
+//   useEffect(() => {
+//     if (savedImageUri) {
+//       entry.imageUri = savedImageUri;
+//     }
+//   }, [savedImageUri]);
+
+//   const handleSave = () => {
+//     const updatedEntry = { ...entry, title: editedTitle };
+//     navigation.goBack();
+//   };
+
+//   return (
+//     <ScrollView contentContainerStyle={[styles.container, { backgroundColor: darkMode ? '#1c1c1c' : 'white' }]}>
+//       <Image source={{ uri: entry?.imageUri || '' }} style={styles.image} />
+//       <TextInput
+//         value={editedTitle}
+//         onChangeText={handleTitleChange}
+//         placeholder="Entry Title"
+//         style={[styles.titleInput, { borderColor: darkMode ? '#ccc' : 'black' }]}
+//       />
+//       <Text style={[styles.dateText, { color: darkMode ? '#666' : 'black' }]}>{entry?.date || ''}</Text>
+//       <Text style={[styles.locationText, { color: darkMode ? '#666' : 'black' }]}>{entry?.location || ''}</Text>
+//       <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
+//         <Text style={[styles.saveButtonText, { color: darkMode ? 'black' : 'white' }]}>Save</Text>
+//       </TouchableOpacity>
+//     </ScrollView>
+//   );
+// };
+
+// EntryScreen.navigationOptions = ({ navigation }) => ({
+//   title: 'Entry Details',
+//   headerRight: () => (
+//     <TouchableOpacity style={{ marginRight: 15 }} onPress={() => navigation.navigate('CameraScreen')}>
+//       <MaterialCommunityIcons name="theme-light-dark" size={24} color="black" />
+//     </TouchableOpacity>
+//   ),
+// });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flexGrow: 1,
+//     padding: 20,
+//     alignItems: 'center',
+//   },
+//   image: {
+//     width: 300,
+//     height: 300,
+//     resizeMode: 'cover',
+//     borderRadius: 5,
+//     marginBottom: 20,
+//   },
+//   titleInput: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//     marginBottom: 10,
+//     padding: 10,
+//     borderWidth: 1,
+//     borderColor: '#ccc',
+//     borderRadius: 5,
+//     width: '100%',
+//   },
+//   dateText: {
+//     fontSize: 16,
+//     marginBottom: 10,
+//   },
+//   locationText: {
+//     fontSize: 16,
+//     marginBottom: 20,
+//   },
+//   saveButton: {
+//     backgroundColor: 'blue',
+//     paddingVertical: 10,
+//     paddingHorizontal: 20,
+//     borderRadius: 5,
+//   },
+//   saveButtonText: {
+//     color: 'white',
+//     fontSize: 16,
+//     fontWeight: 'bold',
+//   },
+// });
+
+// export default EntryScreen;
 
 
 
