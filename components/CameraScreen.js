d85acfd2a32523f5c461354c6b3297fb0903693f
Ryan Eastman <rking1323@gmail.com>
2023-07-19 07:10:41 -0700

    added basic camera page and installed dependencies to access camera and location

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -1,3 +1,109 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Location from 'expo-location';
+
+
+const CameraScreen = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [capturedImage, setCapturedImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      // Check camera permissions
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+
+      // Check location permissions
+      const { status: locationStatus } = await Location.requestForegroundPermissionsAsync();
+      if (locationStatus !== 'granted') {
+        alert('Location permission is required to capture photos.');
+      }
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.takePictureAsync();
+      setCapturedImage(photo);
+    }
+  };
+
+  const saveEntry = () => {
+    if (capturedImage) {
+      // Here, you can implement logic to save the captured image as a new journal entry
+      // along with date and location information.
+      // After saving, navigate back to the HomeScreen.
+      // For simplicity, we'll just navigate back to HomeScreen without saving the image.
+      navigation.navigate('HomeScreen');
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} ref={(ref) => setCameraRef(ref)} type={Camera.Constants.Type.back} />
+      <TouchableOpacity style={styles.captureButton} onPress={takePicture}>
+        <Text style={styles.captureButtonText}>Capture</Text>
+      </TouchableOpacity>
+      {capturedImage && (
+        <TouchableOpacity style={styles.saveButton} onPress={saveEntry}>
+          <Text style={styles.saveButtonText}>Save Entry</Text>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  captureButton: {
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  captureButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  saveButton: {
+    position: 'absolute',
+    bottom: 80,
+    alignSelf: 'center',
+    backgroundColor: 'green',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  saveButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+
+export default CameraScreen;
 
 
 
